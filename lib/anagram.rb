37628858258018654da3603213c1dbd1a42251fa
said shah <saidmysr20@aol.com>
2018-10-14 01:03:07 -0400

    done in one line

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+  require "pry"
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def match(given_array)
+      given_array.select { |each_word| each_word.split("").sort == @word.split("").sort}
+    end
+
+
+
+
+
+
+end
