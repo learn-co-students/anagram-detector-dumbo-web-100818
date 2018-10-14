f22b115eb89961829203f337521efb5ce3b5919f
hmalik88 <hbmalik88@gmail.com>
2018-10-14 00:54:27 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+	attr_accessor :word
+	def initialize(word)
+		@word = word
+	end
+	def match(anagram_array)
+		anagram_array.select do |anagram|
+			if anagram.size == word.size
+				truthiness = word.split("").map {|letter| anagram.include?(letter)}
+				anagram if truthiness.count(true) == anagram.size
+			end
+		end
+	end
+end
